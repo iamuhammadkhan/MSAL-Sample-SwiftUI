2351387cc9c7da822ef7c526b99686cfa567805c
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-24 13:27:09 +0500

    Initial Commit

---
+++ Testing-MSAL-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing-MSAL-SwiftUI
+//
+//  Created by Muhammad Khan on 8/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
